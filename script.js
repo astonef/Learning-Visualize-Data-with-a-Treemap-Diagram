a2bf66cb564f490804a87f36d00024c683c99108
mojo_pin <stefano.g.magni@proton.me>
2023-12-02 20:26:42 +0100

    231202.1.0

--- script.js
+++ script.js
@@ -1,92 +1,169 @@
-let url = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json';
-let movieData;
-let canvas = d3.select('#canvas');
-let tooltip = d3.select("#tooltip"); 
+const width = 1200;
+const height = 800;
 
-let drawTreeMap = () => {
-    let hierarchy = d3.hierarchy(movieData, (node) => {
-        return node['children'];
-    }).sum((node) => {
-        return node['value'];
-    }).sort((node1, node2) => {
-        return node2['value'] - node1['value']; 
+const svg = d3
+  .select("div")
+  .append("svg")
+  .attr("height", height)
+  .attr("width", width);
+
+const colorScale = d3.scaleOrdinal(d3.schemeTableau10);
+
+const tooltip = d3.select("div").append("div").attr("id", "tooltip");
+const url =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => drawDiagram(json));
+
+function drawDiagram(data) {
+  
+  const hierarchy = d3.hierarchy(data);
+
+  
+
+  hierarchy.sum((d) => d.value);
+
+  
+
+  const treemap = d3.treemap();
+
+  const treemapLayout = treemap(hierarchy);
+
+  const games = [];
+
+  for (let i = 0; i < treemapLayout.children.length; i++) {
+    for (let j = 0; j < treemapLayout.children[i].children.length; j++) {
+      games.push(treemapLayout.children[i].children[j]);
+    }
+  }
+
+  svg
+    .selectAll("rect")
+    .data(games)
+    .enter()
+    .append("rect")
+    .attr("class", "tile")
+    .attr("data-name", (d) => d.data.name)
+    .attr("data-category", (d) => d.data.category)
+    .attr("data-value", (d) => d.data.value)
+   
+    .attr("x", (d) => d.x0 * width)
+    .attr("y", (d) => d.y0 * (height - 100))
+    .attr("width", (d) => (d.x1 - d.x0) * width - 0.5)
+    .attr("height", (d) => (d.y1 - d.y0) * (height - 100 - 0.5))
+    .attr("fill", (d) => colorScale(d.data.category))
+    .on("mouseover", function (evt, d) {
+      tooltip.transition().duration(200).style("opacity", 0.9);
+      tooltip.html(
+        "Name: " +
+          d.data.name +
+          "<br>" +
+          "Category: " +
+          d.data.category +
+          "<br>" +
+          "Value: " +
+          d.data.value
+      );
+      tooltip.attr("data-value", d.value);
+
+      tooltip.style("position", "absolute");
+      tooltip.style("left", evt.pageX + 20 + "px");
+      tooltip.style("top", evt.pageY + "px");
+    })
+    .on("mouseout", function () {
+      tooltip.transition().duration(400).style("opacity", 0);
     });
 
-    let createTreeMap = d3.treemap()
-        .size([1500, 600]);
-
-    createTreeMap(hierarchy);
-
-    let movieTiles = hierarchy.leaves();
-
-    let piece = canvas.selectAll('g')
-        .data(movieTiles)
-        .enter()
-        .append('g')
-        .attr("transform", (movie) => { 
-            return 'translate(' + movie['x0'] + ',' + movie['y0'] + ')';
-        });
-
-    piece.append('rect')
-        .attr("class", "tile")
-        .attr("fill", (movie) => { 
-            let category = movie['data']['category'];
-            if (category === "Action") {
-                return "red";
-            } else if (category === 'Drama') {
-                return "purple";
-            } else if (category === 'Adventure') {
-                return "green";
-            } else if (category === 'Comedy') {
-                return "yellow";
-            } else if (category === "Biography") {
-                return 'grey';
-            } else if (category === 'Animation') {
-                return "Orange";
-            } else {
-                return "Steelblue";
-            }
-        }).attr("data-name", (movie) => { 
-            return movie['data']["name"];
-        }).attr("data-category", (movie) => { 
-            return movie['data']["category"];
-        }).attr("data-value", (movie) => { 
-            return movie['data']["value"];
-        }).attr("width", (movie) => { 
-            return movie['x1'] - movie['x0'];
-        }).attr("height", (movie) => { 
-            return movie['y1'] - movie['y0'];
-        });
-    
-    piece.append("text")
-        .text((movie) => {
-            return movie['data']['name'];
-        }).attr('x', 5)
-          .attr('y', 20)
-          .on("mouseover", (movie) => { 
-            tooltip.transition()
-                   .style("visibility", "visible");
-            tooltip.text(() => {
-                return movie['data']['name'] + ' || ' + movie['data']['category'] + ' || ' + movie['data']['value'];
-            });
-            tooltip.attr("data-value", () => {
-                return movie['data']['value'];
-            });
-          })
-          .on("mouseout", () => {
-            tooltip.transition()
-                   .style("visibility", "hidden");
+  svg
+    .selectAll("text")
+    .data(games)
+    .enter()
+    .append("text")
+    .selectAll("tspan")
+    .data((d) => {
+      let splitWords = d.data.name.split(/(?=[A-Z][^A-Z])/g); // split the name of game
+      let res = [];
+      let lines = 0;
+      let rectWidth = (d.x1 - d.x0) * width;
+      let rectHeight = (d.y1 - d.y0) * (height - 100);
+      for (let i = 0; i < splitWords.length; i++) {
+        lines++;
+        if ((lines + 1) * 10 >= rectHeight) {
+          let newText = splitWords.slice(i).join("");
+          res.push({
+            text: newText,
+            x0: d.x0,
+            y0: d.y0,
           });
-};
-
-d3.json(url).then(
-    (data, error) => {
-        if (error) {
-            console.log(error);
+          break;
+        } else if (
+          i !== splitWords.length - 1 &&
+          (splitWords[i].length + splitWords[i + 1].length) * 10 < rectWidth
+        ) {
+          res.push({
+            text: splitWords[i] + splitWords[i + 1],
+            x0: d.x0, // keep x0 reference
+            y0: d.y0, // keep y0 reference
+          });
+          i++;
         } else {
-            movieData = data;
-            drawTreeMap(); 
-            console.log(movieData);
+          res.push({
+            text: splitWords[i],
+            x0: d.x0,
+            y0: d.y0,
+          });
         }
+      }
+      return res;
+      
+    })
+    .enter()
+    .append("tspan")
+    .attr("x", (d) => d.x0 * width + 5)
+    .attr("y", (d, i) => d.y0 * (height - 100) + 10 + i * 9) // offset by index
+    .text((d) => {
+      return d.text;
+    })
+    .attr("font-size", "10px")
+    .attr("font-weight", "bold");
+ 
+
+  let platformsArr = games.map((game) => game.data.category);
+  let platforms = [];
+  platformsArr.map((platform) => {
+    if (!platforms.includes(platform)) {
+      platforms.push(platform);
     }
-);
+  });
+
+  const legend = svg
+    .append("g")
+    .attr("id", "legend")
+    .attr("transform", "translate(0, " + (height - 80) + ")");
+
+  legend
+    .selectAll("rect")
+    .data(platforms)
+    .enter()
+    .append("rect")
+    .attr("class", "legend-item")
+    .attr("width", 35)
+    .attr("height", 25)
+    .attr("x", (d, i) => i * 35)
+    .attr("y", 0)
+    .attr("fill", (d, i) => colorScale(d))
+    .attr("opacity", 0.7);
+
+  legend
+    .selectAll("text")
+    .data(platforms)
+    .enter()
+    .append("text")
+    .attr("x", (d, i) => i * 35.5)
+    .attr("font-size", "10px")
+    .attr("font-weight", "bold")
+    .attr("y", 35)
+    .text((d, i) => d);
+}
